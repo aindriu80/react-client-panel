3ae3ad6d91d635bcc573716e1cd03dac8a9526bf
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-12-28 10:23:29 +0000

    Adding sidebar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import AppNavBar from "./components/layout/AppNavbar";
-
+import Dashboard from "./components/layout/Dashboard";
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
           <AppNavBar />
-          <h1>React front to back</h1>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
         </div>
       </Router>
     );
