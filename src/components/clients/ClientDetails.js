8fda7a8ed1d9558e1ca943ffc2e8c174dc2413ba
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2019-01-01 10:58:18 +0000

    client details with balance and npm classnames

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -5,6 +5,7 @@ import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import Spinner from "../layout/Spinner";
+import classnames from "classnames";
 
 class ClientsDetails extends Component {
   render() {
@@ -12,7 +13,61 @@ class ClientsDetails extends Component {
     if (client) {
       return (
         <div>
-          <h1>{client.firstName}</h1>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-circle-left" /> Back to Dashboard
+              </Link>
+            </div>
+            <div className="col-md-6">
+              <div className="btn-group float-right">
+                <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
+                  Edit
+                </Link>
+                <button className="btn btn-danger">Delete</button>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="card">
+            <h3 className="card-header">
+              {client.firstName}
+              {client.lastName}
+            </h3>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-8 col-sm-6">
+                  <h4>
+                    Client ID:{" "}
+                    <span className="text-secondary">{client.id}</span>
+                  </h4>
+                </div>
+                <div className="col-md-4 col-sm-6">
+                  <h3 className="pull-right">
+                    Balance:{" "}
+                    <span
+                      className={classnames({
+                        "text-danger": client.balance > 0,
+                        "text-success": client.balance === 0
+                      })}
+                    >
+                      €{parseFloat(client.balance).toFixed(2)}
+                    </span>
+                  </h3>
+                  {/* @todo - balanceform */}
+                </div>
+              </div>
+              <hr />
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Contact Email:{client.email}
+                </li>
+                <li className="list-group-item">
+                  Contact Phone: {client.phone}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       );
     } else {
@@ -20,6 +75,10 @@ class ClientsDetails extends Component {
     }
   }
 }
+
+ClientsDetails.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
 
 export default compose(
   firestoreConnect(props => [
