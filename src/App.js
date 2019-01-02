49883a7a593126fe663fd63b55c7bfd839f91d57
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2019-01-02 10:28:29 +0000

    Details Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AppNavBar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import AddClient from "./components/clients/AddClient";
 import ClientDetails from "./components/clients/ClientDetails";
+import EditClient from "./components/clients/EditClient";
 
 class App extends Component {
   render() {
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
               </Switch>
             </div>
           </div>
---
+++ src/components/clients/EditClient.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
+
+class EditClients extends Component {
+  render() {
+    return <h1>Edit</h1>;
+  }
+}
+
+export default EditClients;
